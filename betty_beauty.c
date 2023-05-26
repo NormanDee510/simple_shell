c19b56a192c4c7c115e4e48c21ae294a0248ac75
Aiyans <soureiyanoura@iut-dhaka.com>
2023-05-26 10:46:43 -0700

    Updated for the first task of the project

---
+++ betty_beauty.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *message = "I am an ALX software engineering student doing hard things.";
+
+	printf("%s\n", message);
+
+	return (0);
+}
